53e2655c5151319c4fccf2298954b952738b3b69
محمد <77845409+woejhehejeoeoeoekhehshdd@users.noreply.github.com>
2024-07-23 14:01:28 +0300

    Update troll.py

--- zelz/assistant/troll.py
+++ zelz/assistant/troll.py
@@ -23,10 +23,10 @@ async def on_plug_in_callback_query_handler(event):
                 reply_pop_up_alert = encrypted_tcxt
         except KeyError:
             reply_pop_up_alert = (
-                "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
+                "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات نيـرثون"
             )
     else:
         reply_pop_up_alert = (
-            "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
+            "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات نيـرثون"
         )
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
